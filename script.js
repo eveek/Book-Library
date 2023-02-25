9dc8f451eef3cfee6c29ba2dd49722ba69c01bcb
Victor Enweoru <venweoru@gmail.com>
2023-02-25 12:27:05 +0100

    created the form

---
+++ script.js
@@ -0,0 +1,58 @@
+const authorInput = document.getElementById("bookauthor")
+const titleInput = document.getElementById("booktitle")
+const pagesInput = document.getElementById("bookpages")
+const titleLabel = document.querySelector(".label1")
+const authorLabel = document.querySelector(".label2")
+const pagesLabel = document.querySelector(".label3")
+const formbutton = document.getElementById("fbtn")
+const authorError = document.getElementById("spana")
+const titleError = document.getElementById("spant")
+const pagesError = document.getElementById("spanp")
+
+function addTrans (label){
+    label.classList.add("labeltrans")
+}
+function removeTrans (label){
+    label.classList.remove("labeltrans")
+}
+
+
+authorInput.addEventListener("focus", () => addTrans(authorLabel));
+titleInput.addEventListener("focus", () => addTrans(titleLabel));
+pagesInput.addEventListener("focus", () => addTrans(pagesLabel));
+
+authorInput.addEventListener("blur", () => {
+    if (authorInput.value == ""){
+        removeTrans(authorLabel)
+    }
+})
+titleInput.addEventListener("blur", () => {
+    if (titleInput.value == ""){
+        removeTrans(titleLabel)
+    }
+})
+pagesInput.addEventListener("blur", () => {
+    if (pagesInput.value == ""){
+        removeTrans(pagesLabel)
+    }
+})
+
+formbutton.addEventListener("click", e => {
+    e.preventDefault()
+
+    if(authorInput.value == ""){
+        authorError.style.visibility = "visible"
+    } else {
+        authorError.style.visibility = "hidden"
+    }
+    if(titleInput.value == ""){
+        titleError.style.visibility = "visible"
+    } else {
+        titleError.style.visibility = "hidden"
+    }
+    if(isNaN(pagesInput.value) || pagesInput.value == ""){
+        pagesError.style.visibility = "visible"
+    } else {
+        pagesError.style.visibility = "hidden"
+    }
+})
