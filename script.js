014009280ef155da820ae8a419540e620c145964
Victor Enweoru <venweoru@gmail.com>
2023-05-25 13:13:46 +0100

    Added class

--- script.js
+++ script.js
@@ -50,11 +50,13 @@ function addBookToLibrary( title, author, pages, read){
     displayLibrary()
 }
 
-function Book( title, author, pages, read){
-    this.title = title
-    this.author = author
-    this.pages = pages
-    this.read = read
+class Book {
+    constructor (title, author, pages, read) {
+        this.title = title
+        this.author = author
+        this.pages = pages
+        this.read = read
+    }
 }
 
 
